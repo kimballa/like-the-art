6fb85b318234838e84894c27b556e796d365279e
Aaron Kimball <akimball83@gmail.com>
2022-07-06 17:46:58 -0700

    Add 'why do you love )'( ?' sentence.

--- sentence.cpp
+++ sentence.cpp
@@ -31,7 +31,8 @@ void setupSentences() {
   _sentence( S_WHY | S_DO | S_YOU | S_HATE | S_QUESTION );
   _sentence( S_I | S_LIKE | S_ART | S_BANG );
   _sentence( S_I | S_LOVE | S_ART | S_BANG );
-  _sentence( S_YOU | S_DONT | S_HAVE | S_TO | S_LIKE | S_BM );
+  _sentence( S_YOU | S_DONT | S_HAVE | S_TO | S_LIKE | S_BM | S_BANG );
+  _sentence( S_WHY | S_DO | S_YOU | S_LOVE | S_BM | S_QUESTION );
 }
 
 /** Turn on the signs for sentence (and only those signs) */
